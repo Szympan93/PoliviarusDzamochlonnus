fb827f93e4dcaca279526bba0277400e7e72a627
Kamil Orłow <40249705+orlow-kamil@users.noreply.github.com>
2020-08-29 20:24:02 +0200

    Cleanup

--- PoliviarusDzamochlonnus/Assets/Scripts/GUI/PlayButton.cs
+++ PoliviarusDzamochlonnus/Assets/Scripts/GUI/PlayButton.cs
@@ -1,12 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PlayButton : MonoBehaviour
 {
-    void Start()
+    private void Start()
     {
         GetComponent<Button>().onClick.AddListener(() => SceneManager.LoadScene(1));
         GetComponent<Button>().onClick.AddListener(() => GameManager.Instance.StartGame());
